eba095abbffbe349939160307112225b4d400334
Alan Gutierrez <alan@prettyrobots.com>
2012-01-03 05:09:00 -0500

    Created example process server for testing.
    The example process server is a program that simply checks that signals and file
    handles are rest, then blocks waiting for any character on STDIN.

---
+++ src/t/server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+/* Initially, this setup was going to take place in the attendant library linked
+ * to the host application, after fork and before execve, but the whole thing
+ * felt like a race condition. There are a lot of issues to consider in the
+ * worst cases, 
+ */
+
+/* True if a file handle is open. */
+int is_open(int fd) {
+  return fcntl(fd, F_GETFL) != -1 || errno != EBADF;
+}
+
+/* True if a file handle is a stdio file handle. */
+int is_stdio(int fd) {
+  return fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO;
+}
+
+/* This is a testing server. It quits when it gets any input on stdin. It
+ * reports any unexpected conditions to standard out. It verifies that the
+ * attendant has started the process with a clean environment. It represents a
+ * well behaved library server process. */
+int main() {
+  struct sigaction sig;
+  int signum, fd;
+  char ch;
+  DIR *dir;
+  struct dirent *ent;
+
+  /* Report any signal that is not set to the default signal disposition. */
+  for (signum = SIGHUP; signum < NSIG; signum++) {
+    sigaction(signum, NULL, &sig);
+    if (sig.sa_handler != SIG_DFL) {
+      printf("SIGNAL: %s.\n", strsignal(signum));
+    }
+  }
+
+  /* Report any open file handles other than stdio handles. */
+  dir = opendir("/dev/fd");
+  if (dir == NULL) {
+    printf("FDS: Cannot open /dev/fd.");
+  } else {
+    while ((ent = readdir(dir)) != NULL) {
+      fd = atoi(ent->d_name);
+      if (! is_stdio(fd) && fd != dirfd(dir)) {
+        printf("OPEN: %d.\n", fd);
+      }
+    }
+    closedir(dir);
+  }
+
+  /* Report any stdio handles that are closed. */
+  if (! is_open(STDIN_FILENO))  printf("CLOSED: STDIN");
+  if (! is_open(STDOUT_FILENO)) printf("CLOSED: STDOUT");
+  if (! is_open(STDERR_FILENO)) printf("CLOSED: STDERR");
+
+  /* Termiante on any input. */
+  getchar();
+
+  /* Always successful, even if things weren't as expected, we did our job in
+   * reporting the state. If it is wrong, that is someone else's failure. */
+  return EXIT_SUCCESS;
+}
