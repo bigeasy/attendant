947c000872545ff336e960a242588b2941de2639
Alan Gutierrez <alan@prettyrobots.com>
2012-01-30 05:54:14 -0500

    Test missing server program.

---
+++ src/t/attendant/missing-server.t.c
@@ -0,0 +1,31 @@
+#include <limits.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "../../../errors.h"
+#include "../../../attendant.h"
+#include "../ok.h"
+#include "../../../eintr.h"
+
+void abend() {
+  struct attendant__errors errors = attendant.errors();
+  ok(errors.attendant == RELAY_CANNOT_EXEC, "relay cannot exec");
+  ok(errors.system == ENOENT, "enoent");
+}
+
+int main() {
+  char path[PATH_MAX], **trace;
+  int err;
+  char const * argv[] = { NULL };
+
+  printf("1..2\n");
+  attendant.initialize(strcat(getcwd(path, PATH_MAX), "/relay"), 31);  
+  err = attendant.start(strcat(getcwd(path, PATH_MAX), "/t/bin/server-x"), argv, abend);
+  attendant.ready();
+  attendant.destroy();  
+
+  return EXIT_SUCCESS;
+}
