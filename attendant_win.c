96dd0573e5436bc6c858940edcc94d8fe0b8619d
Alan Gutierrez <alan@blogometer.com>
2012-02-03 06:32:23 -0500

    Create pipes, error handling.

--- attendant_win.c
+++ attendant_win.c
@@ -1,10 +1,18 @@
 #include <Windows.h>
 #include "attendant.h"
+#include "errors.h"
 
 typedef void (*abend_handler_t)();
 
+struct stdio {
+  attendant__pipe_t parent[3];
+  attendant__pipe_t child[3];
+};
+
 struct process_t {
   int instance;
+  struct stdio stdio;
+  struct attendant__errors errors;
 };
 
 static struct process_t process;
@@ -17,7 +25,52 @@ static attendant__pipe_t stdio(int pipe) {
   return NULL;
 }
 
+
+static void free_pipe_handles() {
+  int i;
+  for (i = 0; i < 3; i++) {
+    if (process.stdio.parent[i]) {
+      /* Free handle. */
+    }
+    if (process.stdio.child[i]) {
+      /* Free handle. */
+    }
+  }
+}
+
+
+#define okay(condition, error)                  \
+  do {                                          \
+    if (!condition) {                           \
+        process.errors.attendant = error;       \
+        process.errors.system = GetLastError(); \
+        goto done;                              \
+    }                                           \
+  } while (0);
+
 static int initalize(const char *relay, int canary) {
+  int success;
+  SECURITY_ATTRIBUTES security; 
+
+  security.nLength = sizeof(SECURITY_ATTRIBUTES); 
+  security.bInheritHandle = TRUE; 
+  security.lpSecurityDescriptor = NULL; 
+  
+  success = CreatePipe(&process.stdio.parent[0], &process.stdio.child[0],
+       &security, 0);
+  okay(success, INITIALIZE_CANNOT_CREATE_STDIN_PIPE);
+
+  success = CreatePipe(&process.stdio.child[1], &process.stdio.parent[1],
+       &security, 0);
+  okay(success, INITIALIZE_CANNOT_CREATE_STDOUT_PIPE);
+
+  success = CreatePipe(&process.stdio.child[2], &process.stdio.parent[2],
+       &security, 0);
+  okay(success, INITIALIZE_CANNOT_CREATE_STDERR_PIPE);
+done:
+  if (!success) {
+    free_pipe_handles();
+  }
   return 0;
 }
 
